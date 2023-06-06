60b6aee5b421bdcb62d4c22804869a8db277577b
AhiruHana <hoatthij2006@gmail.com>
2023-06-06 15:44:10 +0700

    sua giao dien va css

---
+++ assets/js/slide.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.slide').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      dots: true,
+  });
+});
